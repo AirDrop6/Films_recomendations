652fc7726e61b26f89ca4317fc2430feaef42344
AirDrop <airdropchyits@gmail.com>
2024-11-12 03:39:43 +0300

    Creating FastAPI app with three get functions

---
+++ src/router.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+from src.init import model
+
+router = APIRouter(
+    prefix='/films'
+)
+
+@router.get('')
+async def get_films():
+    return model.get_films()
+
+@router.get('/search/{title}')
+async def get_film(title: str):
+    return model.get_film(title)
+
+@router.get('/{title}')
+async def get_recommendations(title: str):
+    return model.get_recommendations(title)
