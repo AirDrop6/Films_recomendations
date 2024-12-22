e8517b3b3b25299e503eb3e8ed2d9b96af84d7fb
AirDrop <airdropchyits@gmail.com>
2024-12-22 16:53:21 +0300

    Added new CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -10,7 +10,9 @@ app.include_router(router_films)
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    'http://127.0.0.1:8000'
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:8080',
+    "http://localhost:8080",
 ]
 
 app.add_middleware(
