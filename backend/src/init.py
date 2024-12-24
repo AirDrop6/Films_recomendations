fc6b5ae041c2dfaf5c1b6a8cbdd19c6118571537
AirDrop <airdropchyits@gmail.com>
2024-12-24 13:25:42 +0300

    Fixed backend paths

--- backend/src/init.py
+++ backend/src/init.py
@@ -3,7 +3,7 @@ import pandas as pd
 from src.model import RecommendationModel
 
 
-PATH = 'backend/kinopoisk_top_850.csv'
+PATH = 'kinopoisk_top_850.csv'
 data = pd.read_csv(PATH, index_col=[0])
 combined_features = data['Genre']+''+data['Director']+''+data['Star1']+''+data['Star2']+''+data['Overview']
 
